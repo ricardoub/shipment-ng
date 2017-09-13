150db8f8627394b2f9fcdbf04fa8b1bd11cbf6f2
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-13 09:39:57 -0300

    home page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePageComponent } from './pages/home-page/home-page.component'
+
 const routes: Routes = [
   {
     path: '',
+    component: HomePageComponent,
     children: []
   }
 ];
